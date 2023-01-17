aff90f20f9082ea03de4aab442aa88baad9e3849
nikita <nsimiyu111@gmail.com>
2023-01-17 11:17:27 -0800

    first commit

---
+++ ContactsApi/Controllers/ContactController.cs
@@ -0,0 +1,93 @@
+﻿using ContactsApi.Data;
+using ContactsApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContactsApi.Controllers
+{
+    [ApiController]
+    [Route("api/controller")]
+    public class ContactController : Controller
+    {
+        private readonly ContactApiDbContext contactApiDbContext;
+
+        public ContactController(ContactApiDbContext contactApiDbContext)
+        {
+            this.contactApiDbContext = contactApiDbContext;
+        }
+
+
+        [HttpGet]
+        public IActionResult GetContacts()
+        {
+            contactApiDbContext.Contacts.ToList();
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("{id:guid}")]
+        public IActionResult GetContact([FromRoute] Guid id)
+        {
+            var contact = contactApiDbContext.Contacts.Find(id);
+
+            if (contact == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(contact);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddContact(AddContactRequest addContactRequest)
+        {
+            var contact = new Contact()
+            {
+                Id = Guid.NewGuid(),
+                Address = addContactRequest.Address,
+                Email = addContactRequest.Email,
+                FullName = addContactRequest.FullName,
+                Phone = addContactRequest.Phone
+            };
+            await contactApiDbContext.Contacts.AddAsync(contact);
+            await contactApiDbContext.SaveChangesAsync();
+
+            return Ok(contact);
+        }
+
+        [HttpPut]
+        [Route("{id:guid}")]
+        public IActionResult UpdateContact([FromRoute] Guid id, UpdateContactRequest updateContactRequest)
+        {
+            var contact = contactApiDbContext.Contacts.Find(id);
+            if (contact != null) 
+            {
+                contact.Email = updateContactRequest.Email;
+                contact.FullName = updateContactRequest.FullName;
+                contact.Phone = updateContactRequest.Phone;
+                contact.Address = updateContactRequest.Address;
+
+                contactApiDbContext.SaveChanges();
+
+                return Ok(contact);
+            }
+            return NotFound();
+
+        }
+
+        [HttpDelete]
+        [Route("{id:guid}")]
+        public IActionResult DeleteContact([FromRoute] Guid id)
+        {
+            var contact = contactApiDbContext.Contacts.Find(id);
+            if (contact != null)
+            {
+                contactApiDbContext.Remove(contact);
+                contactApiDbContext.SaveChanges();
+                return Ok(contact);
+            }
+
+            return NotFound();
+        }
+
+    }
+}
---
+++ ContactsApi/Program.cs
@@ -0,0 +1,31 @@
+using ContactsApi.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//Register the database
+builder.Services.AddDbContext<ContactApiDbContext>(options => options.UseInMemoryDatabase("ContactDb"));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
